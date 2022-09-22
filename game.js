1dca86b73b9ea61aba12e9d00ed961062502653f
Lauren Frazier <lauren.e.frazier@gmail.com>
2022-09-22 06:03:20 -0600

    Add Player instances in Game class

--- game.js
+++ game.js
@@ -1,10 +1,20 @@
+var human = new Player ('human', '👤');
+var computer = new Player ('computer', '💻');
+
 class Game {
   constructor() {
     this.compChoice = '';
     this.gameCount = 0;
+    this.classicChoices = ["./assets/rock.png", "./assets/post-it.png", "./assets/scissors.png"];
+    this.competitors = [];
+  }
+  rallyTheTroops(human, computer) {
+    console.log('testing1', human)
+    console.log('testing2', computer)
+    this.competitors.push(human, computer)
   }
   randomCompChoice() {
-    var classicChoices = ["assets/rock.png", "assets/post-it.png", "assets/scissors.png"];
+    // var classicChoices = ["assets/rock.png", "assets/post-it.png", "assets/scissors.png"];
     for (var i = 0; i < classicChoices.length; i++) {
       var randomChoice = classicChoices[Math.floor(Math.random() * classicChoices.length)]
     }
@@ -21,6 +31,15 @@ var testing = new Game ();
 
 
 
+// determineWinner(player1, player2) {
+// if (player1.classicChoices[0] && this.compChoice === 'assets/post-it.png') {h2element.innerHTML = "Computer Wins!"}
+// else if (player1.classicChoices[1] && this.compChoice === 'assets/scissors.png') {h2element.innerHTML = "Computer Wins!"}
+// else if (player1.classicChoices[2] && this.compChoice === 'assets/rock.png') {h2element.innerHTML = "Computer Wins!"}
+
+// else if (player1.classicChoices[2] && this.compChoice === 'assets/post-it.png') {h2element.innerHTML = "Human Wins!"}
+// else if (player1.classicChoices[1] && this.compChoice === 'assets/post-it.png') {h2element.innerHTML = "Computer Wins!"}
+// else if (player1.classicChoices[0] && this.compChoice === 'assets/post-it.png') {h2element.innerHTML = "Human Wins!"}
+// else {return h2element.innerHTML = "It's a draw!" player1.wins = 0  player2.wins = 0}
 
 
 
@@ -42,10 +61,3 @@ var testing = new Game ();
 // if (comp chooses scissors && human chooses rock === true) {human wins}
 
 //update innerHTML of h2 for who wins
-
-
-//method gameCount - default to 0, create method to add gameCount++
-
-
-
-//method randomCompChoice:
--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-var human = new Player ('human', '👤');
-var computer = new Player ('computer', '💻');
+
 
 var classicChoices = ["assets/rock.png", "assets/post-it.png", "assets/scissors.png"]
