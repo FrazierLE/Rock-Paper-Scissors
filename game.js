1c93a4ea35fcbeddeea99c2b85eb9d3cfcff27a2
Lauren Frazier <lauren.e.frazier@gmail.com>
2022-09-21 20:19:05 -0600

    Begin to build Game class and create random computer choice method

---
+++ game.js
@@ -0,0 +1,47 @@
+class Game {
+  constructor() {
+    this.compChoice = '';
+  }
+  randomCompChoice() {
+    var classicChoices = ["assets/rock.png", "assets/post-it.png", "assets/scissors.png"];
+    for (var i = 0; i < classicChoices.length; i++) {
+      var randomChoice = classicChoices[Math.floor(Math.random() * classicChoices.length)]
+    }
+    this.compChoice = randomChoice;
+    return
+  }
+}
+
+var choices = new Game ();
+
+
+
+
+
+
+
+
+//classic game logic :
+// rock > scissors
+//scissors > paper
+// paper > rock
+// if (human chooses rock && computer chooses rock === true) {draw}
+// if (human chooses paper && computer chooses paper === true) {draw}
+// if (human chooses scissors && computer chooses scissors === true) {draw}
+
+// if (human chooses rock && computer chooses paper === true) {comp wins}
+// if (human chooses paper && computer chooses scissors === true) {comp wins}
+// if (human chooses scissors && computer chooses rock === true) {comp wins}
+
+// if (comp chooses rock && human chooses paper === true) {human wins}
+// if (comp chooses paper && human chooses scissors === true) {human wins}
+// if (comp chooses scissors && human chooses rock === true) {human wins}
+
+//update innerHTML of h2 for who wins
+
+
+//method gameCount - default to 0, create method to add gameCount++
+
+
+
+//method randomCompChoice:
--- main.js
+++ main.js
@@ -1,2 +1,4 @@
-var human = new Player ('human', '👤')
-var computer = new Player ('computer', '💻')
+var human = new Player ('human', '👤');
+var computer = new Player ('computer', '💻');
+
+var classicChoices = ["assets/rock.png", "assets/post-it.png", "assets/scissors.png"]
